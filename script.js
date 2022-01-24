acdfb4d326a762bd09b46b3fd240d528eb65826d
Dio Lemos <pedrodiogenes@outlook.com>
2022-01-24 07:21:13 +0000

    Minor changes
    Random is now spelled correctly.
    
    the Result section now only has an empty p element . JS will use this to write the result.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 
 
-function ramdomPercentageGenerator() {
+function randomPercentageGenerator() {
     let firstPerson = Document.getElementById("first-person").innerHTML;
     let seconPerson = Document.getElementById("second-person").innerHTML;
 
