72b1539d062d8e4a69487f7060f8f7b0c4d44098
Dio Lemos <pedrodiogenes@outlook.com>
2022-01-24 07:09:33 +0000

    Update script.js
    created a function called random percentage generator.

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+
+
+function ramdomPercentageGenerator() {
+    let firstPerson = Document.getElementById("first-person").innerHTML;
+    let seconPerson = Document.getElementById("second-person").innerHTML;
+
+
+    let result = Math.floor(Math.random() * 100 + 1);
+}
+
